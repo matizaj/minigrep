a51df9ebb0ea58b2091711b8baba286795010f48
Mateusz <matzaj101@gmail.com>
2025-11-12 15:22:34 +0100

    open test file, small refactor

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,25 @@
 use std::env::args;
+use std::fs;
+
+struct Config {
+    query: String,
+    file_path: String
+}
+
+impl Config {
+    fn new(args: &[String]) -> Self {
+        Config{query: args[1].clone(), file_path: args[2].clone()}
+    }
+}
 fn main() {
     println!("...:::mini-grep:::...");
     let args: Vec<String> = args().collect();
-    let query = &args[1];
-    let file_path = &args[2];
-    println!("Searching for query: {query}");
-    println!("In file: {file_path}");
+    let config = Config::new(&args);
+    println!("Searching for query: {}", config.query);
+    println!("In file: {}", config.file_path);
+
+    let content = fs::read_to_string(config.file_path)
+            .expect("should have been able to read this file");
+
+    println!("{content}");
 }
