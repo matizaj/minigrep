7d5400b68f96a66520a88009a43a8d9b92acfc81
Mateusz <matzaj101@gmail.com>
2025-11-13 21:01:44 +0100

    err msg send to stderr

--- src/main.rs
+++ src/main.rs
@@ -24,12 +24,12 @@ fn main() {
     let args: Vec<String> = args().collect();
 
     let config = Config::build(&args).unwrap_or_else(|err| {
-        println!("problem parsing arguments: {err}");
+        eprintln!("problem parsing arguments: {err}");
         process::exit(1);
     });
 
     if let Err(err) = run(config) {
-         println!("problem parsing arguments: {err}");
+        eprintln!("problem parsing arguments: {err}");
         process::exit(1);
     }
     
